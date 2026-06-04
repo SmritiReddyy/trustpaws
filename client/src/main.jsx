6945d993f235c266464b0d7fc1d9d5913778f3ba
SmritiReddyy <u.smritireddy@gmail.com>
2026-06-04 17:02:22 -0400

    fix: redirect parents page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,16 @@ import { Toaster } from 'react-hot-toast';
 import App from './App';
 import './index.css';
 
-// Handle redirect from 404.html
-const redirect = new URLSearchParams(window.location.search).get('redirect');
-if (redirect) {
-  window.history.replaceState(null, '', '/trustpaws' + redirect);
-}
+// Handle redirect from 404.html (spa redirect pattern)
+(function() {
+  var params = new URLSearchParams(window.location.search);
+  var p = params.get('p');
+  if (p) {
+    var q = params.get('q');
+    var path = '/trustpaws/' + p.replace(/^\//, '') + (q ? '?' + q.replace(/~and~/g, '&') : '');
+    window.history.replaceState(null, '', path);
+  }
+})();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
